bdec1b9219d9465508fa8a978f0b9f397aeef10f
alex <contact@oxydia.com>
2013-01-17 01:14:59 +0100

    Added jstorage for loading categories and articles. Next Step- Load articles from the xml file using ajax

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 function Reader() {
+	this.categories_menu = [];
 	this.categories = [];
 
 	if (typeof Reader.initialized == "undefined" ) {
@@ -23,48 +24,69 @@ function Reader() {
 			});
 		};
 
-		Reader.prototype.refresh = function () {
+		Reader.prototype.full_refresh = function () {
 			this.loadLocal();
 			if(app.is_connected()) {
 				this.loadOnline();
 			}
 			this.saveLocal();
-			alert('fu');
 		};
 
-		Reader.prototype.buildCategoriesMenu = function () {
+		Reader.prototype.rebuildMenu = function () {
+			this.updateCategoriesMenu();
+			$('.categoryBtn').remove();
 
-		};
-
-		Reader.prototype.initialize = function () {
-			this.setListeners();
-
-			$(app.categories).each(function(i, cat) {
+			// @todo : Sort by weight
+			$(this.categories_menu).each(function (i, cat) {
+			// @todo : FIXME : Normally we should loop on $(this.categories)
+			// $(this.categories).each(function (i, cat) {
 				// Add each category to the navbar
 				var $link = $('<a>', {
-					href: "#read?category="+cat.id,
+					href: "read.html?category="+cat,
 					class: "categoryBtn",
-					text: cat.name
+					text: cat
 				});
-				var $li = $('<li>');
-				$link.appendTo($li);
-				$li.appendTo('#categories');
+				$link.appendTo('#categories_menu');
 			});
-			// Fetch categories name
-			// Call buildCategoriesMenu
-			// Refresh
-		}
+		};
 
 		Reader.prototype.setListeners = function () {
 			$('#read .fetchBtn').click(this.refresh);
+		};
+
+		// Update the list of categories
+		Reader.prototype.updateCategoriesMenu = function () {
+			this.categories_menu = ['Home']; // @todo : Init with a parameter from the settings loaded with the app (app.settings.categories_menu ?)
+			if(this.categories_menu.size == 0) {
+				this.categories_menu = $.jStorage.get('categories_menu');
+			}
+			$.jStorage.set('categories_menu', this.categories_menu);
+		};
+
+
+		// Add Home to the pages by default.
+		if (this.categories.size == 0) {
+			this.categories_menu.push("Home");
 		}
 
+		this.rebuildMenu();
+		var weight = 0;
+		var _this = this;
+		$(this.categories_menu).each(function(i, cat_key) {
+			var category = new Category();
+			category.id = cat_key;
+			category.weight = ++weight;
+			category.refresh();
+			_this.rebuildMenu();
+		});
+		// Fetch categories name
+		// Call buildCategoriesMenu
+		// Refresh
+
+		//this.rebuildCategories();
+		this.setListeners();
 		Reader.initialized = true;
 	}
-
-	alert('Reader instanciated');
-	this.initialize();
-	alert('Reader loaded');
 }
 
 function Category(){
@@ -84,7 +106,7 @@ function Category(){
 
 			$.ajax(this.fetch_url, {
 				dataType: 'json', // data will be parsed in json automagically
-				type: "POST",
+				type: "GET",
 				data: articles_ids,
 				cache: false,
 				success: function(json) {
@@ -108,18 +130,28 @@ function Category(){
 					}
 				},
 				error: function() {
-					app.errorOrNoInternet();
-								app.updateHome();
+					app.errorOrNoInternet();;
 				}
 			});
 		};
 
 		// Storage fetch
 		Category.prototype.loadLocal = function() {
+			if (this.id == "home") {
+				this.name = "Home";
+				this.articles = [];
+			} else {
+				var category = $.jStorage.get('categories['+this.id+']');
+				if(category) {
+					this.name = category.name;
+					this.articles = category.articles;
+				}
+			}
 		};
 
 		// Save in local storage for faster refresh
 		Category.prototype.saveLocal = function() {
+			$.jStorage.set('categories['+this.id+']')
 		};
 
 		// Linked to interface
@@ -141,23 +173,22 @@ function Article(){
 	var subhead;
 	var picture;
 	var datetime;
+	var author;
 	var is_read = false;
 	var status = "draft";
 
 	if(typeof Article.initialized == "undefined") {
-
 		// Print article in html
 		Article.prototype.show = function () {
-			$('#article div[data-role="content"]').empty();
-			if(this.picture == null) {
-				$('<div style="width:100%; height:100px; position:relative; background-color:#fbfbfb; border:1px solid #b8b8b8;"><img src="http://codiqa.com/static/images/v2/image.png" alt="image" style="position:absolute; top:50%; left:50%; margin-left:-16px; margin-top:-18px"></div>').appendTo('#article div[data-role="content"]');
+			if(!this.picture) {
+				$('#article .article_img').parent().hide();
+			} else {
+				$('#article .article_img').attr('href', this.picture);
 			}
-			else{
-				$('<img src="'+ article.picture +'" alt="image" style="position:absolute; top:50%; left:50%; margin-left:-16px; margin-top:-18px">').appendTo('#article div[data-role="content"]');
-			}
-		    
-			$('<h2>'+ this.title +'</h2>').appendTo('#article div[data-role="content"]');
-			$('<p>'+ this.subhead +'</p>').appendTo('#article div[data-role="content"]');
+			$('#article .article_body').html(this.body);
+			$('#article .article_title').text(this.title);
+			$('#article .article_author').text(this.author);
+
 		};
 
 		Article.prototype.refresh = function () {
@@ -172,18 +203,20 @@ function Article(){
 
 		};
 
-		Article.prototype.find_by_id = function (article_id) {
-			//return ...
-			var article = new Article();
-			//data = $.jStorage.get('articles['+article_id+']');
-			if(!data) { return null; }
-			
-			var data = "{}";
-			data = $.parseJSON(data);
-			article.id = article_id;
-			article.title = data.title;
-			return article;
-		}
+		Article.prototype.loadLocal = function (id) {
+
+			var fakeData = "{\"id\":"+id+", \"title\":\"Titre\", \"author\":\"Jean-Michel\", \"body\":\"<p>Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb. </p><p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing. </p><p>Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'? </p><p>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man. </p><p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>\"}";
+			$.jStorage.set('articles['+article_id+']', fakeData);
+
+			// For Debug Use Only
+			var article = $.jStorage.get('articles['+article_id+']');
+
+			article = $.parseJSON(article);
+			this.id = article.id;
+			this.title = article.title;
+			this.author = article.author;
+			this.body = article.body;
+		};
 
 		// ...
 
@@ -192,15 +225,13 @@ function Article(){
 }
 
 var app = {
-	page: "read",
+	page: null,
 	last_update: -1,
 
 	categories: ['home'],
 
-
 	settings: {
-		max_article_number: 10,
-		is_logged_in: false
+		max_article_number: 10
 	},
 
 	//settings: new Settings();
@@ -211,8 +242,12 @@ var app = {
 		// fetch config data from storage
 		switch(page) {
 			case 'read' : var reader = new Reader(); break;
-			case 'article': Article.find_by_id(article_id).show(); break;
+			case 'article':
+				var article = new Article();
+				article.loadLocal(213546);
+				article.show(); break;
 			case 'write' : var writer = new Writer(); break;
+			default: alert('no page initialized'); break;
 		}
 	},
 
@@ -223,59 +258,6 @@ var app = {
 	errorOrNoInternet: function() {
 		var message = "Unable to fetch fresh news.";
 		console.log(message);
-	},
-
-	/**
-		Articles
-	*/
-
-	loadArticles: function(event) {
-		// load from storage
-		var last_update = app.last_update;
-		// fetch category by category
-		$(categories).each(function(i, cat) {
-			// fetch from server, sending the current list of articles for limiting the weight of the data sent back ?
-			app.fetchApiArticles(cat);
-		});
-	},
-
-	updateHome: function() {
-		$(categories).each(function(i, cat) {
-			if(app.nextCategory == cat.id) {
-				// clear the list
-				$('#articles').empty();
-				// Update the title
-				$('#app_title').text("Kicknews: " + cat.name);
-				// Add the articles
-				if(cat.articles.length == 0) {
-					var $li = $('<li>');
-					var $link = $('<a>', {
-						href: "#",
-						class: "fetchBtn",
-						text: "Fetch !"
-					});
-
-					$link.attr("data-transition", "slide");
-					$li.attr("data-theme", "c");
-					$link.appendTo($li);
-					$li.appendTo('#articles');
-
-					app.setListeners();
-				} else {
-					$(cat.articles).each(function(i, article) {
-						var $element = $('<li class="article" data-theme="c"><a href="#" data-transition="slide" class="article_title">' + article.title + '</a></li>');
-						$element.hide();
-						$element.appendTo('#articles');
-						$element.show();
-					});
-				}
-				
-				app.last_update = Date.now();
-				app.currentCategory = app.nextCategory;
-				$('#articles').listview('refresh');
-				console.log('Articles updated !');
-			}
-		});
 	},
 
 
